514b1eb0b02c406618ec7dc2d9402a886f3f011e
Nasty12456 <thisisitchief1321@gmail.com>
2024-09-28 08:24:18 -0400

    part 79

---
+++ txData/QBCoreFramework_B2944D.base/resources/[sandy]/rdzk_cs_sandy/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+this_is_a_map 'yes'
+lua54 'yes'
+
+file 'sp_manifest.ymt'
+-- SCENARIO_POINTS_FILE may work, too
+data_file 'SCENARIO_POINTS_OVERRIDE_FILE' 'sp_manifest.ymt'
+
+dependency '/assetpacks'
+
+author "Rudzik"
+description "Sandy Shores - REBORN"
+version "1.1"
+
+dependency '/assetpacks'
