d6df40596c3200b5872d9575d988d6dc07b49af6
Nasty12456 <thisisitchief1321@gmail.com>
2024-09-28 06:35:12 -0400

    Part 23

---
+++ txData/QBCoreFramework_B2944D.base/resources/[clothing]/doitdigital_neon_puffer_jacket/fxmanifest.lua
@@ -0,0 +1,14 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+fx_version 'cerulean'
+author 'KilluaZoldyck#0099'
+description 'Neon Black Puffer Jacket'
+version '1.0.0'
+games { 'gta5' }
+dependency '/assetpacks'
+
+files {
+  'mp_m_freemode_01_mp_m_add2.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_add2.meta'
+dependency '/assetpacks'
