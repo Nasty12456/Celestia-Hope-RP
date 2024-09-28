cb259110f08304f04086768dfda761907576557d
Nasty12456 <thisisitchief1321@gmail.com>
2024-09-28 06:25:16 -0400

    Part 8

---
+++ txData/QBCoreFramework_B2944D.base/resources/[assets_vehicles]/qb-jobvehicle/fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'cerulean'
+game 'gta5'
+ 
+name 'HCRP-vehicles'
+description 'HCRP-vehicles'
+author 'HCRP'
+version '1.0.1'
+ 
+ 
+files {
+    'audioconfig/*.dat151.rel',
+	'audioconfig/*.dat54.rel',
+	'audioconfig/*.dat10.rel',
+	'sfx/**/*.awc',
+    'data/**/*.meta'
+}
+
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/cvpiv8_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/cvpiv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/cvpiv8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_cvpiv8'
