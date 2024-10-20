dacbc88c7454985e662197e7b23ba68262a68972
Nasty12456 <thisisitchief1321@gmail.com>
2024-10-20 08:58:26 -0400

    Clothing Changes Part 5

--- txData/QBCoreFramework_B2944D.base/resources/[clothing]/DoItDigital_Neon_Sweather/fxmanifest.lua
+++
@@ -1,8 +0,0 @@
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
-author 'KilluaZoldyck#0099'
-description 'Do It Digital Neon Sweather'
-fx_version 'cerulean'
-version '1.0.0'
-games { 'gta5' }
-dependency '/assetpacks'
-dependency '/assetpacks'
