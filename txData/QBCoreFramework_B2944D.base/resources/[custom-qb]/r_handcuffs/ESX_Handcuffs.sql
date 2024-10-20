9202f1981e6c92001d9a21e9ddc12c80733c48dc
Nasty12456 <thisisitchief1321@gmail.com>
2024-10-20 08:59:06 -0400

    Remove r-handcuffs

--- txData/QBCoreFramework_B2944D.base/resources/[custom-qb]/r_handcuffs/ESX_Handcuffs.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
-('grinder', 'Grinder', 1, 0, 1),
-('handcuffs', 'Handcuffs', 1, 0, 1),
-('handcuff_keys', 'Handcuff keys', 1, 0, 1),
-('rope', 'Rope', 1, 0, 1),
-('knife', 'Knife', 1, 0, 1);;
