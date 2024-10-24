ab7484d5685bf176daf99e8f13cf364417ff88c6
Nasty12456 <thisisitchief1321@gmail.com>
2024-10-24 14:26:27 -0400

    Create fxmanifest.lua
    Co-Authored-By: Stardawg <61508209+Star-dawg@users.noreply.github.com>

---
+++ txData/QBCoreFramework_B2944D.base/resources/[standalone]/star-scripts/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Star Development'
+description 'Star Scripts'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua',
+    'shared/*.lua'
+}
+
+client_scripts {
+    'client/*.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
+
+lua54 'yes'
