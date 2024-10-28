f2411b76acbaf7e5b28df6d0b01d5bec4073c47b
Noexcusee <Vtcuevas@outlook.com>
2024-10-28 12:23:52 -0700

    test fixing the stash

--- txData/QBCoreFramework_B2944D.base/resources/[qb]/qb-policejob/config.lua
+++ txData/QBCoreFramework_B2944D.base/resources/[qb]/qb-policejob/config.lua
@@ -14,9 +14,9 @@ Config.HandCuffItem = 'handcuffs'
 
 
 
-Config.LicenseRank = -1
+Config.LicenseRank = 1
 
-Config.UseTarget = GetConvar('UseTarget', 'true') == 'true'
+Config.UseTarget = GetConvar('UseTarget', 'false') == 'true'
 Config.Locations = {
     ["duty"] = {
         [1] = vector3(442.68, -981.90, 30.69),
