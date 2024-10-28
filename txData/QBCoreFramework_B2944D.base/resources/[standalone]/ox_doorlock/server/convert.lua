db7cbf768ef55a5eba2cad105ee194b35348b853
Noexcusee <Vtcuevas@outlook.com>
2024-10-28 15:27:27 -0700

    adding jobs to ox doorlocks test #21

--- txData/QBCoreFramework_B2944D.base/resources/[standalone]/ox_doorlock/server/convert.lua
+++ txData/QBCoreFramework_B2944D.base/resources/[standalone]/ox_doorlock/server/convert.lua
@@ -58,7 +58,7 @@ MySQL.ready(function()
 					local groups = door.authorizedJobs or {}
 
 						-- Add your authorized jobs here
-						groups['police'] 
+						groups['police'] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18}
 						groups['ambulance'] 
 						groups['mechanic'] 
 
